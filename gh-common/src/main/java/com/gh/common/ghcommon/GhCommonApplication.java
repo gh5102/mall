2dd79889bc861a2792a28c65fd4ec95c73e8f48e
guohao <459094937@qq.com>
2020-07-13 18:16:08 +0800

    springcloud 分布式初步搭建。

---
+++ gh-common/src/main/java/com/gh/common/ghcommon/GhCommonApplication.java
@@ -0,0 +1,13 @@
+package com.gh.common.ghcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhCommonApplication.class, args);
+    }
+
+}
